e388a73fe938c703a0729666f4b5a31efe8c07c4
L___ T_____ <liamthomas@crescentschool.org>
2016-12-13 13:20:31 -0500

    mineswepper?

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 
+import java.awt.*;
+
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
---
+++ src/Minesweepo.java
@@ -0,0 +1,23 @@
+
+import java.awt.image.BufferedImage;
+
+
+public class Minesweepo 
+{
+    int width = 1, height = 1;
+    private int[][] mineLevel;
+    private int[][] blockLevel;
+    
+    public static void clickTile(int x, int y)
+    {
+        
+    }
+    public void newBoard(int w, int h)
+    {
+        
+    }
+    public BufferedImage getBoard()
+    {
+        BufferedImage board = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+    }
+}
