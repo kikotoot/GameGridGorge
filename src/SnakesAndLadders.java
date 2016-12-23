404532461c2d84514be0884fb8b6d6f47898a081
kikotoot <jacobsalem@crescentschool.org>
2016-12-22 21:15:45 -0500

    empty AI

--- src/SnakesAndLadders.java
+++ src/SnakesAndLadders.java
@@ -1,4 +1,28 @@
 
-public class SnakesAndLadders {
+public class SnakesAndLadders implements GridSetup {
+
+	@Override
+	public void drawGrid() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pOneTurn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pEasyAI() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pHardAI() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/TicTacToeSet.java
+++ src/TicTacToeSet.java
@@ -24,51 +24,374 @@ public class TicTacToeSet extends JPanel implements KeyListener, MouseMotionList
 	BufferedImage imgBackground;
 	BufferedImage[] imgPlayerX = new BufferedImage[5];
 	BufferedImage[] imgPlayerO = new BufferedImage[4];
-	
-	public TicTacToeSet() throws IOException{
-		
-		URL fileURL = getClass().getResource("tic tac toe board.jpg"); 
+
+	public TicTacToeSet() throws IOException {
+
+		URL fileURL = getClass().getResource("tic tac toe board.jpg");
 		imgBackground = ImageIO.read(fileURL);
-		
+
 		fileURL = getClass().getResource("X.jpg");
-		for(int i = 0; i <= 4; i++){
+		for (int i = 0; i <= 4; i++) {
 			imgPlayerX[i] = ImageIO.read(fileURL);
 		}
-		
-		fileURL = getClass().getResource("O.png"); 
-		for(int i = 0; i <= 3; i++){
+
+		fileURL = getClass().getResource("O.png");
+		for (int i = 0; i <= 3; i++) {
 			imgPlayerO[i] = ImageIO.read(fileURL);
 		}
-		
+
 	}
-	
-	//setup a tictactoe board
-	public TicTacToeSet(String startGame){
-		//fill in rows
-		for(int i = 0; i < 3; i++){
-			//fill in columns
-			for(int j = 0; j < 3; j++){
-				playerPosition[i][j] = ' ';
+
+	// setup a tictactoe board
+	public TicTacToeSet(String startGame) {
+		// fill in rows
+		for (int i = 0; i < 3; i++) {
+			// fill in columns
+			for (int j = 0; j < 3; j++) {
+				this.playerPosition[i][j] = ' ';
 			}
 		}
 	}
-	
-	public void drawGrid (){
+
+	public void drawGrid() {
 		g.drawImage(imgBackground, 0, 0, 450, 525, null);
-		//check where moves were made and draw game pieces there
+		// check where moves were made and draw game pieces there
 		int timesXPlayed = 0;
 		int timesOPlayed = 0;
-		//runs through rows
-		for(int i = 0; i < 3; i++){
-			//runs through columns
-			for(int j = 0; j < 3; j++){
-				if(playerPosition[i][j] == 'X'){
-					g.drawImage(imgPlayerX[timesXPlayed], 10 + 90 * (i + 1), 50 + 90 * (j + 1), 175, 175, null);
+		// runs through rows
+		for (int i = 0; i < 3; i++) {
+			// runs through columns
+			for (int j = 0; j < 3; j++) {
+				if (this.playerPosition[i][j] == 'X') {
+					g.drawImage(this.imgPlayerX[timesXPlayed], 10 + 90 * (i + 1), 50 + 90 * (j + 1), 175, 175, null);
 					timesXPlayed++;
-				}
-				else if(playerPosition[i][j] == 'O'){
-					g.drawImage(imgPlayerO[timesOPlayed], 10 + 90 * (i + 1), 50 + 90 * (j + 1), 175, 175, null);
+				} else if (this.playerPosition[i][j] == 'O') {
+					g.drawImage(this.imgPlayerO[timesOPlayed], 10 + 90 * (i + 1), 50 + 90 * (j + 1), 175, 175, null);
 					timesOPlayed++;
+				}
+			}
+		}
+	}
+
+	// in main class have if statement if the person chose AI instead of two
+	// player game
+	// have two different loops (one has p1 turn p2 turn, other has ai turn, p2
+	// turn)
+	// AI goes first always
+	public void turnAI() {
+		if (this.turnCountAI == 0) {
+			this.playerPosition[0][0] = 'X';
+			this.turnCountAI++;
+		}
+		if (this.turnCountAI > 0) {
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+					if (this.playerPosition[i][j] == 'O') {
+						if (i == 0 && j == 1) {
+							if (this.turnCountAI == 1) {
+
+							}
+							else{
+								for (int k = 0; k < 3; i++) {
+									for (int l = 0; l < 3; j++) {
+										if (this.playerPosition[k][l] == 'O' && k != i && l != j) {
+											if (this.turnCountAI == 2){
+												
+											}
+											else{
+												for (int m = 0; m < 3; i++) {
+													for (int n = 0; n < 3; j++) {
+														if (this.playerPosition[m][n] == 'O' && m != k && n != l) {
+															if (this.turnCountAI == 3){
+																
+															}
+															else{
+																for (int o = 0; o < 3; i++) {
+																	for (int p = 0; p < 3; j++) {
+																		if (this.playerPosition[o][p] == 'O' && o != m && p != n) {
+																			if (this.turnCountAI == 4){
+																				
+																			}
+																		}
+																	}
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+						if (i == 0 && j == 2) {
+							if (this.turnCountAI == 1) {
+
+							}
+							else{
+								for (int k = 0; k < 3; i++) {
+									for (int l = 0; l < 3; j++) {
+										if (this.playerPosition[k][l] == 'O' && k != i && l != j) {
+											if (this.turnCountAI == 2){
+												
+											}
+											else{
+												for (int m = 0; m < 3; i++) {
+													for (int n = 0; n < 3; j++) {
+														if (this.playerPosition[m][n] == 'O' && m != k && n != l) {
+															if (this.turnCountAI == 3){
+																
+															}
+															else{
+																for (int o = 0; o < 3; i++) {
+																	for (int p = 0; p < 3; j++) {
+																		if (this.playerPosition[o][p] == 'O' && o != m && p != n) {
+																			if (this.turnCountAI == 4){
+																				
+																			}
+																		}
+																	}
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+						if (i == 1 && j == 0) {
+							if (this.turnCountAI == 1) {
+
+							}
+							else{
+								for (int k = 0; k < 3; i++) {
+									for (int l = 0; l < 3; j++) {
+										if (this.playerPosition[k][l] == 'O' && k != i && l != j) {
+											if (this.turnCountAI == 2){
+												
+											}
+											else{
+												for (int m = 0; m < 3; i++) {
+													for (int n = 0; n < 3; j++) {
+														if (this.playerPosition[m][n] == 'O' && m != k && n != l) {
+															if (this.turnCountAI == 3){
+																
+															}
+															else{
+																for (int o = 0; o < 3; i++) {
+																	for (int p = 0; p < 3; j++) {
+																		if (this.playerPosition[o][p] == 'O' && o != m && p != n) {
+																			if (this.turnCountAI == 4){
+																				
+																			}
+																		}
+																	}
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+						if (i == 1 && j == 1) {
+							if (this.turnCountAI == 1) {
+
+							}
+							else{
+								for (int k = 0; k < 3; i++) {
+									for (int l = 0; l < 3; j++) {
+										if (this.playerPosition[k][l] == 'O' && k != i && l != j) {
+											if (this.turnCountAI == 2){
+												
+											}
+											else{
+												for (int m = 0; m < 3; i++) {
+													for (int n = 0; n < 3; j++) {
+														if (this.playerPosition[m][n] == 'O' && m != k && n != l) {
+															if (this.turnCountAI == 3){
+																
+															}
+															else{
+																for (int o = 0; o < 3; i++) {
+																	for (int p = 0; p < 3; j++) {
+																		if (this.playerPosition[o][p] == 'O' && o != m && p != n) {
+																			if (this.turnCountAI == 4){
+																				
+																			}
+																		}
+																	}
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+						if (i == 1 && j == 2) {
+							if (this.turnCountAI == 1) {
+
+							}
+							else{
+								for (int k = 0; k < 3; i++) {
+									for (int l = 0; l < 3; j++) {
+										if (this.playerPosition[k][l] == 'O' && k != i && l != j) {
+											if (this.turnCountAI == 2){
+												
+											}
+											else{
+												for (int m = 0; m < 3; i++) {
+													for (int n = 0; n < 3; j++) {
+														if (this.playerPosition[m][n] == 'O' && m != k && n != l) {
+															if (this.turnCountAI == 3){
+																
+															}
+															else{
+																for (int o = 0; o < 3; i++) {
+																	for (int p = 0; p < 3; j++) {
+																		if (this.playerPosition[o][p] == 'O' && o != m && p != n) {
+																			if (this.turnCountAI == 4){
+																				
+																			}
+																		}
+																	}
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+						if (i == 2 && j == 0) {
+							if (this.turnCountAI == 1) {
+
+							}
+							else{
+								for (int k = 0; k < 3; i++) {
+									for (int l = 0; l < 3; j++) {
+										if (this.playerPosition[k][l] == 'O' && k != i && l != j) {
+											if (this.turnCountAI == 2){
+												
+											}
+											else{
+												for (int m = 0; m < 3; i++) {
+													for (int n = 0; n < 3; j++) {
+														if (this.playerPosition[m][n] == 'O' && m != k && n != l) {
+															if (this.turnCountAI == 3){
+																
+															}
+															else{
+																for (int o = 0; o < 3; i++) {
+																	for (int p = 0; p < 3; j++) {
+																		if (this.playerPosition[o][p] == 'O' && o != m && p != n) {
+																			if (this.turnCountAI == 4){
+																				
+																			}
+																		}
+																	}
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+						if (i == 2 && j == 1) {
+							if (this.turnCountAI == 1) {
+
+							}
+							else{
+								for (int k = 0; k < 3; i++) {
+									for (int l = 0; l < 3; j++) {
+										if (this.playerPosition[k][l] == 'O' && k != i && l != j) {
+											if (this.turnCountAI == 2){
+												
+											}
+											else{
+												for (int m = 0; m < 3; i++) {
+													for (int n = 0; n < 3; j++) {
+														if (this.playerPosition[m][n] == 'O' && m != k && n != l) {
+															if (this.turnCountAI == 3){
+																
+															}
+															else{
+																for (int o = 0; o < 3; i++) {
+																	for (int p = 0; p < 3; j++) {
+																		if (this.playerPosition[o][p] == 'O' && o != m && p != n) {
+																			if (this.turnCountAI == 4){
+																				
+																			}
+																		}
+																	}
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+						if (i == 2 && j == 2) {
+							if (this.turnCountAI == 1) {
+
+							}
+							else{
+								for (int k = 0; k < 3; i++) {
+									for (int l = 0; l < 3; j++) {
+										if (this.playerPosition[k][l] == 'O' && k != i && l != j) {
+											if (this.turnCountAI == 2){
+												
+											}
+											else{
+												for (int m = 0; m < 3; i++) {
+													for (int n = 0; n < 3; j++) {
+														if (this.playerPosition[m][n] == 'O' && m != k && n != l) {
+															if (this.turnCountAI == 3){
+																
+															}
+															else{
+																for (int o = 0; o < 3; i++) {
+																	for (int p = 0; p < 3; j++) {
+																		if (this.playerPosition[o][p] == 'O' && o != m && p != n) {
+																			if (this.turnCountAI == 4){
+																				
+																			}
+																		}
+																	}
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
 				}
 			}
 		}
@@ -77,31 +400,31 @@ public class TicTacToeSet extends JPanel implements KeyListener, MouseMotionList
 	@Override
 	public void mouseDragged(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
